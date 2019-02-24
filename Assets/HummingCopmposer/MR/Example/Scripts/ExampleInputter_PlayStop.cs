0f857e1b754968c6c367aefc8c35fab5fb43df16
U-VAIO\apple <vinyel422@gmail.com>
2019-02-25 00:39:24 +0900

    Add stick operator and function of dynamic arrangement chord blocks

---
+++ Assets/HummingCopmposer/MR/Example/Scripts/ExampleInputter_PlayStop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class ExampleInputter_PlayStop : MonoBehaviour, IInputClickHandler {
+    GameObject stick;
+    private ChaseObject isPlay;
+    public static bool isPause;
+    // Use this for initialization
+    void Start() {
+        stick = GameObject.Find("Stick");
+        isPlay = stick.GetComponent<ChaseObject>();
+        isPause = false;
+        
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData) {
+        //スクリプト自体を可、不可に
+        isPlay.enabled = !isPlay.enabled;
+        //停止すると、一時停止状態は保持されない
+        isPause = false;
+    }
+}
---
+++ Assets/HummingCopmposer/MR/Example/Scripts/ExampleInputter_SpawnBlock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class ExampleInputter_SpawnBlock : MonoBehaviour, IInputClickHandler {
+    GameObject go;
+    bool isSpawn = false;
+    void Start() {
+        go = GameObject.Find("ScriptObject");
+
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData) {
+        if ( !isSpawn ) {
+            go.GetComponent<DynamicChordBlock>().enabled = true;
+        }
+        isSpawn = true;
+    }
+}
