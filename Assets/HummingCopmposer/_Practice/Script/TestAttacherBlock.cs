e6808756d27e3fae1a02f49e475ac76df5c572dc
U-VAIO\apple <vinyel422@gmail.com>
2019-08-01 14:53:26 +0900

    Adjust collider size of block attacher

--- Assets/HummingCopmposer/_Practice/Script/TestAttacherBlock.cs
+++ Assets/HummingCopmposer/_Practice/Script/TestAttacherBlock.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class TestAttacherBlock : MonoBehaviour {
     GameObject _parent;
     GameObject _otherParent;
-	// Use this for initialization
-	void Start () {
+    bool isAttach = false;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -29,15 +30,31 @@ public class TestAttacherBlock : MonoBehaviour {
     }
     */
     private void OnTriggerStay(Collider other) {
+
         if (other.gameObject.name == "Convex") {
             _parent = transform.root.gameObject;
             _otherParent = other.transform.root.gameObject;
-
+            //if (!isAttach) {
+            //    DoubleSizeCollision(_parent);
+            //}
+            //isAttach = true;
             //_otherParent.transform.parent = _parent.transform;
             _otherParent.transform.position = _parent.transform.position - new Vector3(0, 0.02f, 0);
             _otherParent.transform.rotation = _parent.transform.rotation;
 
             //Debug.Log(this.gameObject.name);
         }
+    }
+    
+    private void DoubleSizeCollision(GameObject aboveBlock) {
+        BoxCollider bc;
+        bc = aboveBlock.GetComponent<BoxCollider>();
+        bc.size = new Vector3(bc.size.x * 1.2f, bc.size.y * 1.2f, bc.size.z * 2);
+        bc.center = new Vector3(bc.center.x, bc.center.y, bc.center.z - bc.size.z / 2);
+    }
+    private void HalfSizeCollision(GameObject aboveBlock) {
+        BoxCollider bc;
+        bc = aboveBlock.GetComponent<BoxCollider>();
+        bc.size = new Vector3(bc.size.x, bc.size.y, bc.size.z * 2);
     }
 }
