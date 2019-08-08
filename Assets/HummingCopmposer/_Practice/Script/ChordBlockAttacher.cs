f299a20e8940ee9ed1f928481f1a09a411651069
U-VAIO\apple <vinyel422@gmail.com>
2019-08-09 00:21:52 +0900

    add function of attaching new block to circle

--- Assets/HummingCopmposer/_Practice/Script/ChordBlockAttacher.cs
+++ Assets/HummingCopmposer/_Practice/Script/ChordBlockAttacher.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ChordBlockAttacher : MonoBehaviour {
     private Rigidbody rigid;
+    private BoxCollider bc;
     // Use this for initialization
     void Start () {
         rigid = GetComponent<Rigidbody>();
-        this.transform.rotation = Quaternion.Euler(-90, 0, 0);
+        bc = this.GetComponent<BoxCollider>();
+        //this.transform.rotation = Quaternion.Euler(-90, 0, 0);
     }
 	
 	// Update is called once per frame
@@ -17,11 +19,13 @@ public class ChordBlockAttacher : MonoBehaviour {
     
     private void OnTriggerStay(Collider other) {
         if ( other.tag == "TargetChordBlock" ) {
-            Debug.Log("Hit"); // ログを表示する
+            //Debug.Log("Hit"); // ログを表示する
                               //rigid.velocity = Vector3.zero;
                               //rigid.angularVelocity = Vector3.zero;
             other.transform.rotation = this.transform.rotation;
-            other.transform.position = this.transform.position;
+            //other.transform.position = this.transform.position;
+            
+            other.transform.position = this.transform.GetChild(0).position + new Vector3(0, 0.012f, 0);
         }
     }
 
